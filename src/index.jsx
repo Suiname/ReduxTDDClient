899614f7be2ca52c55d7b1c640c563e07e4c21eb
Jason Tham <jason.tham@tempus.com>
2016-05-05 11:28:57 -0500

    final bug fix

--- src/index.jsx
+++ src/index.jsx
@@ -12,14 +12,13 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
-
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
 
 const createStoreWithMiddleware = applyMiddleware(
-  remoteActionMiddleware
-)(socket);
+  remoteActionMiddleware(socket)
+)(createStore);
 const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
