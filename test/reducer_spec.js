a61ed348fabd71c8fea7dfc41dff6327215b09e5
Jason Tham <jason.tham@tempus.com>
2016-05-05 14:55:10 -0500

    added logic for keeping track of voting rounds, updated tests

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -67,9 +67,10 @@ describe('reducer', () => {
     }));
   });
 
-  it('handles VOTE by setting hasVoted', () => {
+  it('handles VOTE by setting myVote', () => {
     const state = fromJS({
       vote: {
+        round: 23,
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1}
       }
@@ -79,16 +80,21 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       vote: {
+        round: 23,
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1}
       },
-      hasVoted: 'Trainspotting'
+      myVote: {
+        round: 23,
+        entry: 'Trainspotting'
+      }
     }));
   });
 
-  it('does not set hasVoted for VOTE on invalid entry', () => {
+  it('does not set myVote for VOTE on invalid entry', () => {
     const state = fromJS({
       vote: {
+        round: 23,
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1}
       }
@@ -98,25 +104,31 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       vote: {
+        round: 23,
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1}
       }
     }));
   });
-  
-  it('removes hasVoted on SET_STATE if pair changes', () => {
+
+  it('removes myVote on SET_STATE if pair changes', () => {
     const initialState = fromJS({
       vote: {
+        round: 23,
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1}
       },
-      hasVoted: 'Trainspotting'
+      myVote: {
+        round: 23,
+        entry: 'Trainspotting'
+      }
     });
     const action = {
       type: 'SET_STATE',
       state: {
         vote: {
-          pair: ['Sunshine', 'Slumdog Millionaire']
+          round: 24,
+          pair: ['Sunshine', 'Trainspotting']
         }
       }
     };
@@ -124,8 +136,23 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       vote: {
-        pair: ['Sunshine', 'Slumdog Millionaire']
+        round: 24,
+        pair: ['Sunshine', 'Trainspotting']
       }
     }));
   });
+
+  it('doesnt change state on VOTE if vote is not in current pair', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Sunshine'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(state);
+  });
+
 });
